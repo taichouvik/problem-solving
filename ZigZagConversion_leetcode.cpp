91e6f57a906fc236a67b798636283ba3f3056f09
taichouvik <taichouvik@users.noreply.github.com>
2019-11-02 22:26:03 +0530

    removed extra calculations

--- ZigZagConversion_leetcode.cpp
+++ ZigZagConversion_leetcode.cpp
@@ -12,82 +12,52 @@ We just need to to find all the indices at each level for our 'ans'
 
 Pattern:
 at level 0:   the offset between indices is 4 (=nr in the code)
-              so the indices are:   0 , 4= (0+4) or (0+nr)
+so the indices are:   0 , 4= (0+4) or (0+nr)
 at level 1: the offset starts from nr-2 (=var_offset in the code)
-            the offset alternates btw var_offset & nr-var_offset
-              nr-2 = 2
-              so the indices are:   1 , 3= (1+2) or (1+var_offset) , 5= (3+2) or (3+(nr-var_offset))
+the offset alternates btw var_offset & nr-var_offset
+nr-2 = 2
+so the indices are:   1 , 3= (1+2) or (1+var_offset) , 5= (3+2) or (3+(nr-var_offset))
 at level 2: (special case for last level) 
-            the offset again becomes 'nr'
-            so the indices are:     2, 6= (2+nr) or (2+4)
-
-How many steps can we take at each level?
-ANS= maxCol ( calculated iteratively using simple maths)
-
+the offset again becomes 'nr'
+so the indices are:     2, 6= (2+nr) or (2+4)
 */
 
 class Solution {
 public:
 string convert(string chk, int numRows) {
-  
-  //trivial cases
-  if(numRows==0)                                   
-      return "";
-  if(numRows==1)
-      return chk;
-  
-  //calculation for maxCol ie maximum columns of all level
-  int nr=2*(numRows-1);                            
-	int offset=nr;         
-	int maxCol=0;
-	int matchSize=0;
-	int j=-1;
-  
-	while(matchSize<chk.size())
-	{
-    j++;
-    switch(j%2){
-      case 0:
-      {
-          matchSize+=numRows;
-          maxCol+=1;
-          break;
-      }
-      case 1:
-      {
-          matchSize+=numRows-2;
-          maxCol+=numRows-2;
-      }
-    }
-	}
-  
-	if(matchSize>chk.size())
-		if(j%2)
-			maxCol=maxCol-(matchSize-chk.size());
 
-	string ans="";
-   
-  //appending to 'ans' the correct chars of every level 
- 	for(int level=0;level<numRows;level++)
- 	{
-      int var_offset=offset;
-      int steps=0;
-      int i=level;
-      
-      while( steps++ < maxCol and i< chk.size() )
-      {
-          ans.push_back(chk[i]);
-          i+=var_offset;
-          if(var_offset != nr)
-          var_offset=nr-var_offset;
-      }
-      
-      offset-=2;
-      if(offset<=0)
-        offset=nr;
- 	}
-  
- 	return ans;
-  
- }
+//trivial cases
+if(numRows==0)                                   
+return "";
+if(numRows==1)
+return chk;
+
+int nr=2*(numRows-1);                            
+int offset=nr;         
+
+string ans="";
+
+//appending to 'ans' the correct chars of every level 
+for(int level=0;level<numRows;level++)
+{
+	int var_offset=offset;
+	int steps=0;
+	int i=level;
+
+	while( steps++ < maxCol and i< chk.size() )
+	{
+		ans.push_back(chk[i]);
+		i+=var_offset;
+		if(var_offset != nr)
+		var_offset=nr-var_offset;
+	}
+
+	offset-=2;
+	if(offset<=0)
+	offset=nr;
+}
+
+return ans;
+
+}
 };
